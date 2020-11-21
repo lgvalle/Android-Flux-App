1ef44e4b33943673a03bcf9ffc3d7e227fff598f
logwee <77112512@qq.com>
2020-11-21 14:59:33 +0800

    Update TodoActivity.java

--- app/src/main/java/lgvalle/com/fluxtodo/TodoActivity.java
+++ app/src/main/java/lgvalle/com/fluxtodo/TodoActivity.java
@@ -76,6 +76,7 @@ public class TodoActivity extends AppCompatActivity {
         mainList.setLayoutManager(new LinearLayoutManager(this));
         listAdapter = new TodoRecyclerAdapter(actionsCreator);
         mainList.setAdapter(listAdapter);
+        if(mainInput != null){}
         mainInput.hash();
     }
 
